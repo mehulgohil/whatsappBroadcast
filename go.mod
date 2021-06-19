f19cd0c60ad684507a7ed7a301f97221ad460eb7
Mehul Gohil <mehulgohil75@gmail.com>
2021-06-20 00:22:33 +0530

    Session changes

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.16
 require (
 	github.com/Baozisoftware/qrcode-terminal-go v0.0.0-20170407111555-c0650d8dff0f
 	github.com/Rhymen/go-whatsapp v0.1.1
+	github.com/kataras/golog v0.1.7 // indirect
 	github.com/mattn/go-colorable v0.1.8 // indirect
 	github.com/mattn/go-isatty v0.0.13 // indirect
 	github.com/skip2/go-qrcode v0.0.0-20200617195104-da1b6568686e // indirect
--- main.go
+++ main.go
@@ -1,52 +1,138 @@
 package main
 
 import (
+	"encoding/gob"
 	"fmt"
 	qrcodeTerminal "github.com/Baozisoftware/qrcode-terminal-go"
 	"github.com/Rhymen/go-whatsapp"
+	"github.com/kataras/golog"
 	"os"
 	"time"
 )
 
-var contacts = []string{"919167908060", "918080908057", "919769908060", "919324498562", "919768187108"}
+const MessageToSend = "Hey Palavian's\n\nReady to eat Fresh Fruits daily...?!\n\nWe are here to bring you Quality and Fresh Fruits Daily...\n\nOrder Now...\n\n• Anjeer - 100/Box\n\n• Apple - ₹120/Kg\n\n• Apple Royal Gala- ₹225/Kg\n\n• Mosambi - ₹140/kg\n\n• Plum - ₹250/Kg\n\n• Orange - ₹100/kg\n\n• Chiku- ₹80/kg\n\n• Anar- ₹120/kg\n\n• Custer Apple- ₹140/Kg\n\n• Pear(S.A)- ₹240/kg\n\n• Water Melon - ₹60 pc\n\n• Musk Melon - ₹60 Pc\n\n• Sweetcorn - ₹15/Pc\n\n• Litchi - ₹300/kg (Min 1 Kg)\n\n• Dates(Khaarek) - ₹100/Kg\n\n• Jamun - ₹300/Kg (Min 1Kg)\n\n• Elaichi Banana - ₹70/Dz\n\n• Dragon Fruit - ₹50/Pc\n\n• Kiwi - ₹50/Box(3pc)\n\n• Papaya - ₹40/Pc\n\n• Peach - ₹200/kg\n\n• Green Badam - ₹250/Kg\n\n• Kesar Mango - ₹550/ 5kg (Min 5 Kg)\n\n• Langda Mango - ₹450/ 5kg\n\n• Dasheri Mango - ₹450/ 5Kg\n\n\n\nPayment Mode :\nGpay/UPI Number -\n+91 9167908057\n\nContact Person -\nAshwin Gohil\n+91 9323430427\n\nEat Healthy, Stay Healthy...!\n🍏🍎🍐🍊🍌🍉🍇\U0001FAD0🍒🍑\U0001F96D\n\n**Free Home Delivery\n\nhttps://chat.whatsapp.com/JsqUz5Ej8yTGOEnkj1fOlp"
+var contacts = []string{"919768187108", "919769908060"}
 
 func main(){
 
+	//=========New Connection
 	wac, err := whatsapp.NewConn(30 * time.Second)
 	if err != nil {
 		panic(err)
 	}
 	wac.SetClientVersion(2, 2121, 6)
 
+	err = login(wac)
+	if err != nil {
+		golog.Error(err)
+	} else {
+		golog.Info("login Successful")
+	}
+
+	<-time.After(3 * time.Second)
+
+	//=========Looping through contacts and sending messages
+	for i:=0;i<len(contacts);i++{
+
+		img, err := os.Open("frusion.jpeg")
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "error reading file: %v\n", err)
+			os.Exit(1)
+		}
+
+		msg := whatsapp.ImageMessage{
+			Info: whatsapp.MessageInfo{
+				RemoteJid: fmt.Sprintf("%s@s.whatsapp.net", contacts[i]),
+			},
+			Type:    "image/jpeg",
+			Caption: MessageToSend,
+			Content: img,
+		}
+
+		msgId, err := wac.Send(msg)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "error sending message: %v", err)
+			os.Exit(1)
+		} else {
+			fmt.Println("Message Sent -> ID : " + msgId)
+		}
+
+		//<-time.After(1 * time.Second)
+		//fmt.Println("Sending Message to ",contacts[i])
+		//
+		//text := whatsapp.TextMessage{
+		//	Info: whatsapp.MessageInfo{
+		//		RemoteJid: fmt.Sprintf("%s@s.whatsapp.net", contacts[i]),
+		//	},
+		//	Text: MessageToSend,
+		//}
+		//
+		//x, err := wac.Send(text)
+		//fmt.Println(x)
+		//if err!=nil {
+		//	fmt.Println(err)
+		//}
+	}
+}
+
+func login(wac *whatsapp.Conn) error {
+
+	session, err := readSession()
+	if err == nil {
+		session, err = wac.RestoreWithSession(session)
+		if err == nil {
+			return nil
+		}
+	}
+
+	//==========New QR
 	qr := make(chan string)
 	go func() {
 		terminal := qrcodeTerminal.New()
 		terminal.Get(<-qr).Print()
 	}()
 
-	session, err := wac.Login(qr)
+	//=========Passing the QR scan to login
+	session, err = wac.Login(qr)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "error during login: %v\n", err)
-	}
-	fmt.Printf("login successful, session: %v\n", session)
-
-	for i:=0;i<len(contacts);i++{
-		fmt.Println("Sending Message to ",contacts[i])
-
-		text := whatsapp.TextMessage{
-			Info: whatsapp.MessageInfo{
-				RemoteJid: fmt.Sprintf("%s@s.whatsapp.net", contacts[i]),
-			},
-			Text: "Hello Whatsapp, Testing",
-		}
-
-		x, err := wac.Send(text)
-		fmt.Println(x)
-		if err!=nil {
-			fmt.Println(err)
-		}
+		return fmt.Errorf("error during login: %v\n", err)
 	}
 
-	time.Sleep(1000 * time.Second)
+	err = writeSession(session)
+	if err!=nil {
+	   	return fmt.Errorf("error saving session: %v\n", err)
+	}
 
+	return nil
 }
+
+func readSession() (whatsapp.Session, error) {
+	session := whatsapp.Session{}
+	file, err := os.Open("./whatsappSession.gob")
+	if err != nil {
+		return session, err
+	}
+	defer file.Close()
+	decoder := gob.NewDecoder(file)
+	err = decoder.Decode(&session)
+	if err != nil {
+		return session, err
+	}
+	return session, nil
+}
+
+func writeSession(session whatsapp.Session) error {
+	file, err := os.Create("./whatsappSession.gob")
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+	encoder := gob.NewEncoder(file)
+	err = encoder.Encode(session)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+
